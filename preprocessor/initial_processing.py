cc53fbddd8f087e2f3f5b43e6a94453f82901faf
YosefGu <yos770yos@gmail.com>
2025-09-07 17:51:44 +0300

    update preprocessor

--- preprocessor/initial_processing.py
+++ preprocessor/initial_processing.py
@@ -2,6 +2,7 @@ import hashlib
 import sys
 from pathlib import Path
 import time
+from datetime import datetime
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,22 +10,33 @@ base_root = Path(__file__).resolve().parent.parent
 sys.path.append(str(base_root))
 
 from kafka_conn.subscriber import Subscriber
+from elastic import elastic_conn
+from mongodb.mongodb_conn import MongodbClient 
+
 
 
 class InitialProcessing():
 
     def __init__(self):
         self.kafka_conn = Subscriber(os.getenv('METADATA_TOPIC'))
+        self.mongodb_client = MongodbClient()
         self.run()
 
     def run(self):
         try:
+            elastic_conn.initialize()
             while True:
+                doc_list = []
                 data = self.pull_data_from_kafka()
-                if data:
-                    metadata = data.value['metadata']
+                for record in data:
+                    metadata = record.value['metadata']
+                    metadata = self.convert_to_datetime(metadata)
                     unique_id = self.generate_unique_id(metadata)
-
+                    metadata['unique_id'] = unique_id
+                    doc_list.append(metadata)
+                    self.save_data_on_mongodb(metadata['path'])
+                    break
+                self.save_metadata_on_elasticsearch(doc_list)
                 time.sleep(15)
 
                 
@@ -40,15 +52,21 @@ class InitialProcessing():
         unique_id = hashlib.md5(f'{size}{name}{creation_date}'.encode('utf-8')).hexdigest()
         return unique_id
 
+    def convert_to_datetime(self, metadata):
+        format_string = "%Y-%m-%d %H:%M:%S"
+        datetime_object = datetime.strptime(metadata['creation_date'], format_string)
+        metadata['creation_date'] = datetime_object
+        return metadata
+    
     def pull_data_from_kafka(self):
         return self.kafka_conn.get_consumer_event()
 
-    def save_metadata_on_elasticsearch(self, metadata):
-        pass
+    def save_metadata_on_elasticsearch(self, doc_list):
+        elastic_conn.insert_data(doc_list)
 
-    def save_data_on_mongodb(self, data):
-        pass
-
+    def save_data_on_mongodb(self, path):
+        with open(path, 'rb') as f:
+            self.mongodb_client.save_data(f, 'file1')
 
 if __name__ == "__main__":
     InitialProcessing()
