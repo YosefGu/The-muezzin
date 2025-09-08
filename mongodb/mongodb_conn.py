f5bf84288fb33b8165fdef8d5d01e8acfa0b3a82
YosefGu <yos770yos@gmail.com>
2025-09-08 16:49:24 +0300

    add log

--- mongodb/mongodb_conn.py
+++ mongodb/mongodb_conn.py
@@ -3,11 +3,27 @@ import os
 from gridfs import GridFS
 
 class MongodbClient():
+    _instance = None
 
-    def __init__(self):
-        self.client = MongoClient(os.getenv('MONGODB_CONN'))
-        self.database = self.client['muezzin']
-        self.fs = GridFS(self.database, collection='audio')
+    def __get_mongodb_instance(cls):
+        if cls._instance:
+            return cls._instance
+        cls._instance = cls._initialize()
+        return cls._instance
+        # cls._mongo_client =  MongoClient(os.getenv('MONGODB_CONN'))
+        # database = cls._mongo_client['muezzin']
+        # fs = GridFS(database, collection='audio')
+
+    def _initialize():
+        client =  MongoClient(os.getenv('MONGODB_CONN'))
+        db = client['muezzin']
+        fs = GridFS(db, collection='audio')
+        return {"client": client, "db": db, "fs": fs}
+
+    # def __init__(self):
+        # self.client = MongoClient(os.getenv('MONGODB_CONN'))
+        # self.database = self.client['muezzin']
+    #     self.fs = GridFS(self.database, collection='audio')
 
     
     def save_file(self, file, unique_id):
