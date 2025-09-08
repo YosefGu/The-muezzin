a50ffe2c2d91d862f978a2732944c09cf9507340
YosefGu <yos770yos@gmail.com>
2025-09-08 16:50:26 +0300

    add log

--- metadata/extraction.py
+++ metadata/extraction.py
@@ -10,26 +10,27 @@ base_root = Path(__file__).resolve().parent.parent
 sys.path.append(str(base_root))
 
 from kafka_conn.publisher import Publisher
+from logger import Logger
+
+my_logger = Logger.get_logger()
 
 class Extraction():
 
     def __init__(self):
         self.root_folder = os.getenv('ROOT_FOLDER')
-        self.kafka_conn = Publisher()
-        self.run()
     
     def run(self):
         try:
             for file_name in os.listdir(self.root_folder):
-                json_result = self.create_json_metadata(file_name)
+                json_result = self.__create_json_metadata(file_name)
                 json_result["metadata"]["path"] = f'{self.root_folder}\{file_name}'
-                self.publish_to_kafka(json_result)
+                self.__publish_to_kafka(json_result)
+            my_logger.info("The metadata of all audio files has been published successfully.")
         except Exception as e:
-            print("Error: ", str(e))
-            return {"Error: ", str(e)}
- 
-           
-    def create_json_metadata(self, file_name):
+            my_logger.error(f"Error publishing metadata to kafka.\nError:{e}")
+            return str(e)
+          
+    def __create_json_metadata(self, file_name):
         meta_data = os.stat(f'{self.root_folder}/{file_name}')
         json_result = {
             "metadata": {
@@ -40,8 +41,8 @@ class Extraction():
         }
         return json_result
     
-    def publish_to_kafka(self, json_result):
-        return self.kafka_conn.publish_data(os.getenv('METADATA_TOPIC'), json_result)
+    def __publish_to_kafka(self, json_result):
+        return Publisher.publish_data(os.getenv('METADATA_TOPIC'), json_result)
         
 if __name__ == "__main__":
-    Extraction()
+    Extraction().run()
