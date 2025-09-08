e7fa648ef01164e0fc0705eb4fab90f36e6fcdbd
YosefGu <yos770yos@gmail.com>
2025-09-08 16:51:34 +0300

    add log

--- mongodb/mongodb_conn.py
+++ mongodb/mongodb_conn.py
@@ -1,32 +1,23 @@
 from pymongo import MongoClient
 import os
 from gridfs import GridFS
+from logger import Logger
+
+my_logger = Logger.get_logger()
 
 class MongodbClient():
-    _instance = None
-
-    def __get_mongodb_instance(cls):
-        if cls._instance:
-            return cls._instance
-        cls._instance = cls._initialize()
-        return cls._instance
-        # cls._mongo_client =  MongoClient(os.getenv('MONGODB_CONN'))
-        # database = cls._mongo_client['muezzin']
-        # fs = GridFS(database, collection='audio')
-
-    def _initialize():
-        client =  MongoClient(os.getenv('MONGODB_CONN'))
-        db = client['muezzin']
-        fs = GridFS(db, collection='audio')
-        return {"client": client, "db": db, "fs": fs}
-
-    # def __init__(self):
-        # self.client = MongoClient(os.getenv('MONGODB_CONN'))
-        # self.database = self.client['muezzin']
-    #     self.fs = GridFS(self.database, collection='audio')
-
+    
+    def __init__(self):
+        self.client = MongoClient(os.getenv('MONGODB_CONN'))
+        self.database = self.client['muezzin']
+        self.fs = GridFS(self.database, collection='audio')
+        my_logger.info("MongoDB connection completed successfully.")
     
     def save_file(self, file, unique_id):
-        file_id = self.fs.put(file, metadata={"unique_id": unique_id})
-        return {"file_id": file_id}
-    
+        try:
+            file_id = self.fs.put(file, metadata={"unique_id": unique_id})
+            my_logger.info("Data saved successfully.")
+            return {"file_id": file_id}
+        except Exception as e:
+            my_logger.error(f"Error saving data to mongodb.\nError:{e}")
+            return str(e)
